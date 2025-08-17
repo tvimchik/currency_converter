a298f8b1b1e224853f3ac26da36e43656715df44
Robert <timothymatveev1204@gmail.com>
2025-08-17 15:13:58 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+if not API_KEY:
+    print('Ключ не найден!!!')
+else:
+    print('Ключ загружен...')
+
+url = f'https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD'
+response = requests.get(url)
+
+if response.status_code == 200:
+    print('Все работает!')
+    date = response.json()
+    print(f'Пример курса: 1 USD = {date['conversion_rates'] ['EUR']} EUR')
+else:
+    print(f'API НЕ ОТВЕЧАЕТ: КОД ОШИБКИ {response.status_code}')
+    print(f'Сообщение от сервера: {response.text}')
